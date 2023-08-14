322f86cdcd1fb37d9e1fa7d03c416f81e38553d2
xiefei <xie0729@yeah.net>
2023-08-14 09:45:40 +0800

    fix:修改vue3外部点击指令

--- src/vue/directives/clickOutside.ts
+++ src/vue/directives/clickOutside.ts
@@ -1,17 +1,28 @@
-export const vClickOutside = {
-  mounted: (el: HTMLElement, { value }) => {
-    (e: MouseEvent) => {
-      document.addEventListener('click', (e: MouseEvent) => {
-        if (el.contains(e.target as HTMLElement)) {
-          if (typeof value === 'function') {
-            value('inside')
-          }
-        } else {
-          if (typeof value === 'function') {
-            value('outside')
-          }
-        }
-      })
+function pointerHandler(e: MouseEvent, el: any) {
+  const cb = el.__cb;
+  if (el.contains(e.target as HTMLElement)) {
+    if (typeof cb === "function") {
+      cb(false);
+    }
+  } else {
+    if (typeof cb === "function") {
+      cb(true);
     }
   }
+}
+
+export const vClickOutside = {
+  mounted: (el: HTMLElement, { value: cb }) => {
+    (e: MouseEvent) => {
+      (el as any).__cb = cb;
+      document.addEventListener("pointerdown", (e: MouseEvent) =>
+        pointerHandler(e, el)
+      );
+    };
+  },
+  unmounted: (el: HTMLElement) => {
+    document.removeEventListener("pointerdown", (e: MouseEvent) =>
+      pointerHandler(e, el)
+    );
+  },
 };
